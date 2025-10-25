2ec499b6e441241faa6f808de6ba5f8d2a7714ac
Bilal Ahmad Khan <bh2448.hb@gmail.com>
2025-10-25 16:58:03 +0500

    Created Empty Project using Cookiecutter and added UV package manager

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from jeroti!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='An smart OS which identify OS of any hardware by fetching its data',
+    author='Bilal Ahmad Khan',
+    license='',
+)
